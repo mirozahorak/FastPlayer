4b8f66893901ad89d6ce486e37d2449ee47a28be
mirozahorak <mirozahorak@users.noreply.github.com>
2026-02-10 00:22:38 +0100

    Initial Commit

---
+++ FastPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FastPlayer
+//
+//  Created by Miroslav Zahorak on 2/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FastPlayer/FastPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastPlayerApp.swift
+//  FastPlayer
+//
+//  Created by Miroslav Zahorak on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct FastPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
